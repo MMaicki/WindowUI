78ea351deebfad2c2eb98efca4bd7480f66069f9
Marty <mtyrio@gmail.com>
2019-08-04 18:29:17 +0200

    Changed names for hooks functions
    Changed function name from handleX to setX.

--- src/components/Document/document.js
+++ src/components/Document/document.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 
 const Document = () => {
-    const [mouseX, handleMouseX] = useState(0)
-    const [mouseY, handleMouseY] = useState(0)
-    const [menuDisplay, handleMenuDisplay] = useState('none')
-    const [holdMenu, handleHoldMenu] = useState(false)
-    const [isMenuOpened, handleMenuOpenState] = useState('MENU_CLOSED')
-    const [popupDisplay, handlePopupDisplay] = useState('none')
+    const [mouseX, setMouseX] = useState(0)
+    const [mouseY, setMouseY] = useState(0)
+    const [menuDisplay, setMenuDisplay] = useState('none')
+    const [holdMenu, setHoldMenu] = useState(false)
+    const [isMenuOpened, setMenuOpenState] = useState('MENU_CLOSED')
+    const [popupDisplay, setPopupDisplay] = useState('none')
 
     const handleMouseMovement = (evt) => {
-        if(!holdMenu){
-            handleMouseX(evt.clientX)
-            handleMouseY(evt.clientY)
+        if (!holdMenu) {
+            setMouseX(evt.clientX)
+            setMouseY(evt.clientY)
         }
     }
 
@@ -19,17 +19,17 @@ const Document = () => {
         evt.preventDefault()
         switch (isMenuOpened) {
             case 'MENU_OPENED':
-                handleHoldMenu(true)
-                handleMenuOpenState('MENU_CLOSED')
-                handleMouseX(evt.clientX)
-                handleMouseY(evt.clientY)
+                setHoldMenu(true)
+                setMenuOpenState('MENU_CLOSED')
+                setMouseX(evt.clientX)
+                setMouseY(evt.clientY)
                 break
             case 'MENU_CLOSED':
-                handleHoldMenu(true)
-                handleMenuDisplay('block')
-                handleMenuOpenState('MENU_OPENED')
-                handleMouseX(evt.clientX)
-                handleMouseY(evt.clientY)
+                setHoldMenu(true)
+                setMenuDisplay('block')
+                setMenuOpenState('MENU_OPENED')
+                setMouseX(evt.clientX)
+                setMouseY(evt.clientY)
                 break
             default:
                 console.log('default')
@@ -37,24 +37,24 @@ const Document = () => {
     }
 
     const handleMenuClose = (evt) => {
-        handleHoldMenu(false)
-        handleMenuDisplay('none')
-        handleMenuOpenState('MENU_CLOSED')
+        setHoldMenu(false)
+        setMenuDisplay('none')
+        setMenuOpenState('MENU_CLOSED')
     }
 
     const handleWrapperClick = (evt) => {
-        if (isMenuOpened === 'MENU_OPENED' || isMenuOpened === 'MENU_CLOSED'){
+        if (isMenuOpened === 'MENU_OPENED' || isMenuOpened === 'MENU_CLOSED') {
             handleMenuClose()
         }
     }
 
     const handleSampleAction = () => {
         handleMenuClose()
-        handlePopupDisplay('block')
+        setPopupDisplay('block')
     }
 
     const closePopup = () => {
-        handlePopupDisplay('none')
+        setPopupDisplay('none')
     }
 
     return (
@@ -66,8 +66,8 @@ const Document = () => {
                 <button onClick={handleSampleAction}>Sample Action</button>
                 <button onClick={handleMenuClose} style={{ position: 'fixed', display: `${menuDisplay}`, top: `${mouseY - 10}px`, left: `${mouseX + 110}px`, color: '#fff' }}>X</button>
             </div>
-            <div style={{ display: `${popupDisplay}`, position: 'fixed', top: '40vh', left: '40%', width: '20%', height: '20vh', backgroundColor: '#1f1f1f'}}>
-                SAMPLE POPUP XD 
+            <div style={{ display: `${popupDisplay}`, position: 'fixed', top: '40vh', left: '40%', width: '20%', height: '20vh', backgroundColor: '#1f1f1f' }}>
+                SAMPLE POPUP XD
                 <button onClick={closePopup}>Close Popup</button>
             </div>
         </React.Fragment>
